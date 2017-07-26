7e53901a5572b13600ea91550b447c00403c64a3
OuroborosDmk <532409991@qq.com>
2017-07-26 16:38:51 +0800

    123

---
+++ js/starpingfen.js
@@ -0,0 +1,13 @@
+var shopstar=document.getElementById("shopstar");
+alert("0");
+var num=3;
+var stars=shopstar.children();
+alert(stars.length);
+function changestar(){
+	for(var i in stars){
+
+	}
+}
+
+var starpoint=document.getElementById("starpoint");
+starpoint.innerHTML="目前&nbsp;"+num+"分";
